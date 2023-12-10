fc84aab7e4a8a040aa1f4d34459bedbb4c9bcb13
Stanislav Simko <stanislav.simko@gmail.com>
2023-12-10 22:13:06 +0100

    added Secrets.h for MQTT credentials and server settings

--- Secrets.h
+++ Secrets.h
@@ -1,1 +1,6 @@
-
+//#define MQTT_SERVER Set_Your_MQTT_Server_Here //  Fill in below, and delete this line
+//#define MQTT_SERVER "192.168.1.1"
+//#define MQTT_PORT 1883
+//#define MQTT_USER  Set_Your_MQTT_User_Name_Here // Fill in below and delete this line
+//#define MQTT_USER "username"
+//#define MQTT_PASS "password"
